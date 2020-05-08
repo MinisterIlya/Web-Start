0550bb506be22e1b6ad129abfcdd1ec1a2822602
Ilya Gusev <uestd-99@mail.ru>
2020-05-08 19:57:43 +0300

    feat: add scroll up script for logo

--- js/scrollButtons.js
+++ js/scrollButtons.js
@@ -3,4 +3,7 @@ $(document).ready(function () {
     $('html, body').animate({scrollTop: $('.header').height() + $('.hero').height() - 24}, 600)
     return false
   })
+  $('.logo__link').click(function () {
+    $('html, body').animate({scrollTop: 0}, 600)
+  })
 })
