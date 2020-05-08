554748b10d4a562c9f5115719af7aeee9e9e3ac0
Ilya Gusev <uestd-99@mail.ru>
2020-05-08 19:33:39 +0300

    feat: add script for scroll down button

--- js/scrollDown.js
+++ js/scrollDown.js
@@ -1,3 +1,6 @@
 $(document).ready(function () {
-  
+  $('.hero__scroll-down').click(function() {
+    $('html, body').animate({scrollTop: $('.header').height() + $('.hero').height() - 24}, 600)
+    return false
+  })
 })
