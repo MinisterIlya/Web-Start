45ce45dbd876224a64a446be07292e0e9cfbfa12
Ilya Gusev <uestd-99@mail.ru>
2020-05-14 10:45:56 +0300

    feat: add scroll up button

--- js/scrollButtons.js
+++ js/scrollButtons.js
@@ -6,4 +6,16 @@ $(document).ready(function () {
   $('.logo__link').click(function () {
     $('html, body').animate({scrollTop: 0}, 600)
   })
+
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 100) {
+      $('.scrollup').fadeIn();
+    } else {
+      $('.scrollup').fadeOut();
+    }
+  })
+  $('.scrollup').click(function(){
+    $("html, body").animate({ scrollTop: 0 }, 600);
+    return false;
+    });
 })
