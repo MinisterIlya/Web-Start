bff0899254b44d4fa0ea18b3452422cf18581997
Ilya Gusev <uestd-99@mail.ru>
2020-05-15 16:34:31 +0300

    refact: make responsive layout for projects

--- js/scrollButtons.js
+++ js/scrollButtons.js
@@ -8,11 +8,16 @@ $(document).ready(function () {
   })
 
   $(window).scroll(function() {
-    if ($(this).scrollTop() > 100) {
-      $('.scrollup').fadeIn();
+    if ($(window).width() > 960) {
+      if ($(this).scrollTop() > 100) {
+        $('.scrollup').fadeIn();
+      } else {
+        $('.scrollup').fadeOut();
+      }
     } else {
-      $('.scrollup').fadeOut();
+      $('.scrollup').css('display', 'none')
     }
+
   })
   $('.scrollup').click(function(){
     $("html, body").animate({ scrollTop: 0 }, 600);
